3350aa8e587788e20faca44c88df47b781c879d9
john petfield <johnmeatjohnmeat@gmail.com>
2023-01-26 13:58:32 +0000

    Remove comments

--- ASCII_art_project/Program.cs
+++ ASCII_art_project/Program.cs
@@ -8,37 +8,18 @@ namespace ASCII_art_project
 {
     class Program
     {
-        /// <summary>
         /// https://www.youtube.com/watch?v=55iwMYv8tGI&t=275s
         /// https://play.ertdfgcvb.xyz/
-        /// </summary>
-        /// <param name="args"></param>
         static void Main(string[] args)
         {
-            //const string density = "Ñ@#W$9876543210?!abc;:+=-,._ ";
             const string density = "Ñ@#W$9876543210?!abc;:+=-,._ ";
-
-            //string s = @"C:\Users\johnm\source\repos\ASCII_art_solution\images\test3.jpg";
-            string s = @"C:\Users\johnm\source\repos\ASCII_art_solution\images\ted1.jpg";
-            //string s = @"C:\Users\johnm\source\repos\ASCII_art_solution\images\bear2.jpg";
-            //string s = @"C:\Users\johnm\source\repos\ASCII_art_solution\images\test2.jpg";
-            //string s = @"C:\Users\johnm\source\repos\ASCII_art_solution\images\test1.jpg";
-            //string s = @"C:\Users\johnm\source\repos\ASCII_art_solution\images\testdogs.jpg";
-            //string s = @"C:\Users\johnm\source\repos\ASCII_art_solution\images\k.jpg";
-            //string s = @"C:\Users\johnm\source\repos\ASCII_art_solution\images\GoogleLogo.jpg";
-            //string s = @"C:\Users\johnm\source\repos\ASCII_art_solution\images\255red.png";
+            string s = @"C:\Users\johnm\source\repos\ASCII_art_solution\images\bear1.jpg";
 
             bool inverted = false;
 
             Image i = Image.FromFile(s);
 
-            Console.WriteLine($"width: {i.Width} height: {i.Height}");
-
-            //List<Color> ColourArray = GetColourArray(i);
-
             ImageContainer imageContainer = GetColourArray(i);
-
-            Console.WriteLine($"min colour {imageContainer.max} max colour {imageContainer.max}");
 
             int widthCount = 0;
 
@@ -57,19 +38,7 @@ namespace ASCII_art_project
                     index = Math.Floor(Map(greyScaleColour, 0, 255,  density.Length - 1, 0));
                 else
                     index = Math.Floor(Map(greyScaleColour, 0, 255, 0, density.Length - 1));
-                
 
-                /*
-                if(inverted)
-                    index = Math.Floor(Map(greyScaleColour, imageContainer.min, imageContainer.max,  density.Length - 1, 0));
-                else
-                    index = Math.Floor(Map(greyScaleColour, imageContainer.min, imageContainer.max, 0, density.Length - 1));
-                */
-
-                //string charToAdd = density[(int)index] == ' ' ? "&nbsp" : density[(int)index];
-                //line += density[(int)index];
-
-                //Console.WriteLine((int)index);
                 line += density[(int)index] == ' ' ? "&nbsp" : density[(int)index];
 
                 if (widthCount == i.Width)
@@ -81,8 +50,6 @@ namespace ASCII_art_project
                 }
             }
 
-            Console.WriteLine("before write to file");
-
             using (StreamWriter writetext = new StreamWriter(@"C:\Users\johnm\source\repos\ASCII_art_solution\images\output.txt"))
             {
                 foreach(string linetext in output)
@@ -91,11 +58,8 @@ namespace ASCII_art_project
                 }
             }
         }
-        /// <summary>
-        /// 
+
         /// https://forum.unity.com/threads/re-map-a-number-from-one-range-to-another.119437/
-        /// </summary>
-        /// <returns></returns>
         public static float Map(float value, float from1, float to1, float from2, float to2)
         {
             return (value - from1) / (to1 - from1) * (to2 - from2) + from2;
@@ -118,7 +82,6 @@ namespace ASCII_art_project
 
                         colors.Add(pxl);
 
-                        // this is done twice and is lazy
                         int greyScaleColour = (pxl.R + pxl.G + pxl.B) / 3;
                         imageContainer.min = Math.Min(imageContainer.min, greyScaleColour);
                         imageContainer.max = Math.Max(imageContainer.max, greyScaleColour);
@@ -126,10 +89,8 @@ namespace ASCII_art_project
                 }
             }
 
-
             imageContainer.colours = colors;
             return imageContainer;
         }
-
     }
 }
